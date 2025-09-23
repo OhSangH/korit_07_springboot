0f3a9c7ac6485c28d124df0e29e5c79e8f5aaed0
OhSangH <dhtkdgus126@gmail.com>
2025-09-23 17:50:29 +0900

    feat : 250923 spring lesson

--- cardatabase/src/main/java/com/example/cardatabase/domain/Owner.java
+++ cardatabase/src/main/java/com/example/cardatabase/domain/Owner.java
@@ -1,11 +1,14 @@
 package com.example.cardatabase.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
 import lombok.*;
 
 import java.util.*;
 
 @Entity
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 @Data
 @NoArgsConstructor(force = true)
 @RequiredArgsConstructor
@@ -18,6 +21,7 @@ public class Owner {
     private String firstName, lastName;
 
     // 소유자는 다수의 차들을 가징수 있기 때문에 Collections를 사용
+    @JsonIgnore
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
     private List<Car> cars;
 
